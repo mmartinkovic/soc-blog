f6128daf3f2589b3abc1633300dd271f13898de3
martinkovicm <matej.martinkovicc@gmail.com>
2025-04-08 22:11:28 +0200

    Mensie upravy
    Uprava tlacidla Pridat Clanok, vymazanie Domov z navbaru a pridanie Favicons.

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,10 @@ require_once 'functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog System</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
 </head>
 <body>
     <header>
@@ -18,7 +22,6 @@ require_once 'functions.php';
                     <a href="/index.php">Blog System</a>
                 </div>
                 <ul class="nav-links">
-                    <li><a href="/index.php">Domov</a></li>
                     <?php if (isLoggedIn()): ?>
                         <?php if (isAdmin()): ?>
                             <li><a href="/admin/dashboard.php">Admin Panel</a></li>
